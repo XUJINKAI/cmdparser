9bb8821bbc5bce4683ec3836d7dd6100e49594b1
XUJINKAI <XUJINKAI@users.noreply.github.com>
2022-11-12 10:48:32 +0800

    Apache License 2.0

--- cmdparser.c
+++ cmdparser.c
@@ -1,8 +1,11 @@
-/* 
-Command parser library
-Author: XUJINKAI
-Url: https://github.com/XUJINKAI/cmdparser
-LICENSE: MIT
+/*
+Copyright 2022 github.com/XUJINKAI/cmdparser. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
  */
 
 #include "cmdparser.h"
